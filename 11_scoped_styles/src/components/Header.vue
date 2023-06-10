792e1c22e6b8e9bc154f98afc45391b8dd222c48
Gilson Coloni Filho <colonig@hotmail.com>
2023-06-09 21:12:52 -0300

    Create CSS global e scoped

---
+++ 11_scoped_styles/src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Projeto</a></li>
+        <li><a href="#">Contatos</a></li>
+    </ul>    
+</template>
+
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
+
+<style scoped>   /* STYLE só para esse elemento */
+    ul {
+        list-style: none;
+        display: flex;
+        background: #111;
+    }
+
+    li {
+        margin-right: 10px;
+    }
+
+    a {
+        color: #ddd;
+        text-decoration: none;
+        transition: .5s;
+    }
+
+    a:hover {
+        color: turquoise;
+    }
+</style>
---
+++ 11_scoped_styles/src/components/Info.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento</p>
+        <p v-else>Estou em busca de novas oportunidades!</p>
+        <p>Utilizo as seguintes tecnologias</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>Python</li>
+            <li>PHP</li>
+        </ul>
+        <div>
+            <button @click="showEmail">{{ textoBotao }}</button>
+        </div>
+        <p v-show="mostrar_email">Mande mensagem para: {{ email }}</p>
+        <p class="teste">Para acessar meu portifólio <a v-bind:href="meu_link" target="blank">basta clicar aqui</a></p>
+        <Picture />
+    </div>
+</template>
+
+<script>
+import Picture from './Picture.vue'
+    
+export default {  
+    name: 'Info',
+    components: { 
+        Picture
+    },
+    data() {
+        return{
+            esta_trabalhando: false, 
+            mostrar_email: false,
+            email: 'gilson@email.com',
+            meu_link: 'https://google.com',
+            textoBotao: 'Mostrar e-mail'
+        }
+    },
+    methods: {
+        showEmail() {
+            this.mostrar_email = !this.mostrar_email
+            if(!this.mostrar_email) {
+                this.textoBotao = 'Mostrar e-mail'
+            } else {
+                this.textoBotao = 'Ocultar e-mail'
+            }
+        }
+    }
+}
+</script>
+
+<style>              /* STYLE GLOBAL DE BAIXO PARA CIMA  - não fazer , gera bagunça */
+    .paragrafo_pai {
+        color: blue;
+    }
+</style>
