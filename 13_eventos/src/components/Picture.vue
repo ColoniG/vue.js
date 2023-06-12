ecb3ef802ce9c56bb7af3e55ab7d451a0f25616c
Gilson Coloni Filho <colonig@hotmail.com>
2023-06-12 13:04:28 -0300

    Create  13 Eventos

---
+++ 13_eventos/src/components/Picture.vue
@@ -0,0 +1,14 @@
+<template>
+    <img :src="avatar" :alt="descricao">  <!-- :src... = v-bind:src... -->
+</template>
+<script>
+    export default {
+        name: 'Picture',
+        data() {
+            return{
+                avatar: '/img/avatar.png',
+                descricao: 'Gilson Coloni Filho'
+            }
+        }
+    }
+</script>
