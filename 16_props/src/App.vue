5fd78b4acc575357608c022659e784e0b3c917c4
Gilson Coloni Filho <colonig@hotmail.com>
2023-06-15 18:41:32 -0300

    Create 16 Props

---
+++ 16_props/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1 style="font-weight: bolder;">Utilizando Props</h1>
+    <br><br>
+    
+    <Header :esta_logado="true" />
+    <PrimeiroComponente />
+    <p class="paragrafo_pai">O dado de app é: {{ testando }}</p>
+    <LifeCycle />
+    <Pessoa />
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import LifeCycle from './components/LifeCycle.vue'
+  import Pessoa from './components/Pessoa.vue'
+  
+  
+  export default {
+    name: 'App', 
+    components: {
+      Header,
+      PrimeiroComponente,
+      LifeCycle,
+      Pessoa      
+    },
+    
+    data() {
+      return {
+        testando: "Teste"
+      }
+    }
+  }
+</script>
+
+<style>           /* STYLE GLOBAL */
+  body {
+    background-color: #333;
+    color: #fff;
+  }
+
+  a {
+    color: red;
+  }
+
+  .teste {
+    background-color: black;
+  }
+</style>
