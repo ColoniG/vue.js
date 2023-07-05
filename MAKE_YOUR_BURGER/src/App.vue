595863a17b89209830ec2d33d23ee428892c8ff1
Gilson Coloni Filho <colonig@hotmail.com>
2023-07-04 21:23:33 -0300

    Update
    Finalizando cabeçalho e rodapé

--- MAKE_YOUR_BURGER/src/App.vue
+++ MAKE_YOUR_BURGER/src/App.vue
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar.vue';
 
 <template>
   <div>
-    <Navbar />
+    <Navbar :logo="logo_src" :alt="app_name" />
     <router-view/>
     <Footer />
   </div>
@@ -17,10 +17,33 @@ export default {
   components: {
     Navbar,
     Footer
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger"
+    }
   }
 }
 </script>
 
 <style>
+  * {
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
